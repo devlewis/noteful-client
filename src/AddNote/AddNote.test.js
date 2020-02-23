e1590a0b9f1cc603fe1cd7fbcd408506e4ec65b7
Devree Lewis <devree.lewis@gmail.com>
2020-02-23 10:25:31 -0500

    AddNote.test.js in progress

--- src/AddNote/AddNote.test.js
+++ src/AddNote/AddNote.test.js
@@ -3,9 +3,24 @@ import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import AddNote from "./AddNote";
 
+let folders = [
+  {
+    id: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Important"
+  },
+  {
+    id: "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Super"
+  },
+  {
+    id: "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Spangley"
+  }
+];
+
 describe(`AddNote component`, () => {
   it("renders without error", () => {
-    const wrapper = shallow(<AddNote />);
+    const wrapper = shallow(<AddNote folderName={folders} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
