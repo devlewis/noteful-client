79ab1784e2a22f2a10d5df0979a728045e94c82a
Devree Lewis <devree.lewis@gmail.com>
2020-02-25 08:20:27 -0500

    doMock option commented out

--- src/AddNote/AddNote.test.js
+++ src/AddNote/AddNote.test.js
@@ -4,8 +4,63 @@ import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import AddNote from "./AddNote";
 
+// beforeEach(() => {
+//   jest.resetModules();
+// });
+
+// const makeFolderNamesWithContext = (
+//   context = [
+//     {
+//       id: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+//       name: "Important"
+//     },
+//     {
+//       id: "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+//       name: "Super"
+//     },
+//     {
+//       id: "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
+//       name: "Spangley"
+//     }
+//   ]
+// ) => {
+//   jest.doMock("../Context", () => {
+//     return {
+//       Context: {
+//         Consumer: props => props.children(context)
+//       }
+//     };
+//   });
+
+//   return require("./AddNote").AddNote;
+// };
+
+const context = [
+  {
+    id: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Important"
+  },
+  {
+    id: "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Super"
+  },
+  {
+    id: "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
+    name: "Spangley"
+  }
+];
+
 describe(`AddNote component`, () => {
   it("renders without errors", () => {
     expect(shallow(<AddNote />)).toMatchSnapshot();
+  });
+
+  it("renders folders we pass it", () => {
+    // const folderNames = makeFolderNamesWithContext();
+    // console.log(folderNames.debug());
+    const wrapper = shallow(<AddNote />, { context });
+    let options = wrapper.find("option");
+    console.log(options.debug());
+    expect(options).toHaveLength(4);
   });
 });
